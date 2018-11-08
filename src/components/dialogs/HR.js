32656c1356a5a567e1a27a7d1c2f38d97de2958e
Nike <nike.klaubert@gmail.com>
2018-11-08 10:50:49 +0100

    Added the customer satisfaction chart into the HR Tab

--- src/components/dialogs/HR.js
+++ src/components/dialogs/HR.js
@@ -1,9 +1,84 @@
 import React from "react"
 import Modal from "react-modal"
+import {Line} from 'react-chartjs-2'
 import { RadioGroup, RadioButton } from 'react-radio-buttons'
 import VisibleEmployeeList from '../../containers/VisibleEmployeeList'
 import VisibleEmployeeHirePopover from '../../containers/VisibleEmployeeHirePopover'
 import { SHOW_AVAILABLE, SALESPEOPLE_TYPE, ENGINEER_TYPE } from './../../constants/HRConstants'
+
+const data = {
+  labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+  datasets: [{
+    label: 'unhappy',
+    lineTension: 0,
+    backgroundColor: "red",
+    data: [{
+      y: 65
+    },{
+      y: 50
+    },{
+      y: 30
+    },{
+      y: 60
+    }]
+}, {
+  label: 'unsure',
+  lineTension: 0,
+  backgroundColor: "yellow",
+  data: [{
+    x: 0,
+    y: 10
+  },{
+    x: 0,
+    y: 30
+  },{
+    x: 0,
+    y: 30
+  },{
+    x: 0,
+    y: 10
+  }]
+}, {
+  label: 'happy',
+  lineTension: 0,
+  backgroundColor: "green",
+  data: [{
+    x: 0,
+    y: 25
+  },{
+    x: 0,
+    y: 20
+  },{
+    x: 0,
+    y: 40
+  },{
+    x: 0,
+    y: 30
+  }]
+}]
+};
+
+const options = {
+
+  title: {
+    display: true,
+    text: 'Customer Satisfaction'
+  },
+  tooltips: {
+    mode: 'index',
+    intersect: false
+  },
+  responsive: true,
+  scales: {
+    xAxes: [{
+      stacked: true,
+    }],
+    yAxes: [{
+      stacked: true
+    }]
+  }
+}
+
 
 class HR extends React.Component {
 
@@ -49,6 +124,7 @@ class HR extends React.Component {
                     </div>
                     <div className="quarter panel">
                         <h4>Employee Satisfaction</h4>
+                        <Line data={data} options={options} />
                     </div>
                 </div>
             </Modal>
