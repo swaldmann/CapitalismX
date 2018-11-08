6c1c23205fd448c16ffb1d13de4ee2e344c13254
Steffen Waldmann <swaldman@mail.uni-mannheim.de>
2018-11-08 03:33:04 +0100

    Simplified employee reducer

--- src/reducers/employees.js
+++ src/reducers/employees.js
@@ -8,30 +8,15 @@ import { EMPLOYEES } from '../constants/HRConstants'
 const initialState = EMPLOYEES
 
 export default function employees(state = initialState, action) {
-    // TODO: Use just one type for employees
     switch (action.type) {
         case HIRE_EMPLOYEE:
-            if (Array.isArray(state)) {
-                return state.map(employee =>
-                    employee.index === action.index ? { ...employee, isEmployed: true } : employee
-                )
-            }
-            else {
-                return Object.assign({}, ...Object.keys(state).map(k => ({[k]: state[k].map(employee =>
-                    employee.index === action.index ? { ...employee, isEmployed: true } : employee
-                )})))
-            }
+            return Object.assign({}, ...Object.keys(state).map(k => ({[k]: state[k].map(employee =>
+                employee.index === action.index ? { ...employee, isEmployed: true } : employee
+            )})))
         case FIRE_EMPLOYEE:
-        if (Array.isArray(state)) {
-            return state.map(employee =>
-                employee.index === action.index ? { ...employee, isEmployed: false } : employee
-            )
-        }
-        else {
             return Object.assign({}, ...Object.keys(state).map(k => ({[k]: state[k].map(employee =>
                 employee.index === action.index ? { ...employee, isEmployed: false } : employee
             )})))
-        }
         default:
         return state
     }
