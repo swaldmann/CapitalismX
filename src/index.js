221fde9271b8d1cc0c1f560d0dfb83cf5f3fcfb9
Steffen Waldmann <swaldman@mail.uni-mannheim.de>
2018-11-13 18:05:27 +0100

    Added a few basic simulations

--- src/index.js
+++ src/index.js
@@ -7,19 +7,27 @@ import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import rootReducer from './reducers'
-import {advanceTime} from './actions'
 import { getProductUtilities } from './selectors/products'
-import { getAllEmployees } from './selectors/employees'
+import { getAllEngineers, getAllSalespeople, getAllEmployees } from './selectors/employees'
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk))
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+    /*compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )*/
+)
 
 function startSimulation() {
     return (dispatch) => {
         setInterval(function() {
             const state = store.getState()
+            const totalCosts = getAllEmployees(state).reduce((totalSalaries, employee) => employee.isEmployed ? totalSalaries + employee.salary : totalSalaries, 0).toFixed(0)
+            const totalRevenue = getAllEngineers(state).reduce((totalEngineerPoints, engineer) => engineer.isEmployed ? totalEngineerPoints + engineer.skill : totalEngineerPoints, 0) * getProductUtilities(state).reduce((totalUtility, utility) => totalUtility += utility, 0).toFixed(0)
             dispatch({ type: 'START_SIMULATION' })
-            dispatch({ type: 'MAKE_REVENUE', revenue: getProductUtilities(state).reduce((totalUtility, utility) => totalUtility += utility, 0) })
-            //dispatch({ type: 'PAY_COSTS', costs: getAllEmployees(state).reduce((totalSkills, utility) => totalUtility += ugtility, 0) })
+            dispatch({ type: 'MAKE_REVENUE', revenue: totalRevenue })
+            dispatch({ type: 'PAY_COSTS', costs: (totalCosts/365).toFixed(0) })
         }, 3000);
     }
 }
