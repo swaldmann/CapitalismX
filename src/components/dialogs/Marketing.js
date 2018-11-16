8ff34e70302c74c09daf56aebd2e91f685a352f0
Steffen Waldmann <swaldman@mail.uni-mannheim.de>
2018-11-16 01:25:46 +0100

    Added Marketing view

--- src/components/dialogs/Marketing.js
+++ src/components/dialogs/Marketing.js
@@ -22,6 +22,47 @@ class Marketing extends React.Component {
                     <button onClick={this.closeModal} className="dialogClose"><i className="fas fa-times fa-2x"></i></button>
                 </div>
                 <div className="dialogDetail">
+                    <div className="quarter panel">
+                        <h3>Internal</h3>
+                        <h4>Online courses</h4>
+                    </div>
+                    <div className="half flexbox">
+                        <div className="third column-flexbox">
+                            <h3>Campaigns</h3>
+                            <div className="borderedList remaining-height">
+                                <ul>
+                                    <li><img className="icon" src={require('../../static/icons/icons8-news.png')} />Green Company<div className="detailTitle">30 days ago</div></li>
+                                    <li><img className="icon" src={require('../../static/icons/icons8-tv.png')} />Diversity<div className="detailTitle">50 days ago</div></li>
+                                </ul>
+                            </div>
+                            <button className="centered">Start Campaign</button>
+                        </div>
+                        <div className="third column-flexbox">
+                            <h3>Press Releases</h3>
+                            <div className="borderedList remaining-height">
+                                <ul>
+                                    <li>TV: Green Company</li>
+                                    <li>Newspaper: Green Company</li>
+                                </ul>
+                            </div>
+                            <button className="centered">Public Statement</button>
+                        </div>
+                        <div className="third column-flexbox">
+                            <h3>Market Research</h3>
+                            <div className="borderedList remaining-height">
+                                <ul>
+                                    <li>TV: Green Company</li>
+                                    <li>Newspaper: Green Company</li>
+                                </ul>
+                            </div>
+                            <button className="centered">Conduct Research</button>
+                        </div>
+                    </div>
+                    <div className="quarter panel">
+                        <h3>Public Relations</h3>
+                        <h4>Lobbying</h4>
+                        <h4>Management Consultancy</h4>
+                    </div>
                 </div>
             </Modal>
         )
