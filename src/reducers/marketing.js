1c2b7187e868dcdb773c5a20583bdc3fad162718
Steffen Waldmann <swaldman@mail.uni-mannheim.de>
2018-11-18 00:18:32 +0100

    Removed unncecessary Marketing reducer code

--- src/reducers/marketing.js
+++ src/reducers/marketing.js
@@ -2,19 +2,7 @@ import {
     START_CAMPAIGN
 } from '../constants/ActionTypes'
 
-import { MARKETING, CAMPAIGNS } from '../constants/MarketingConstants'
-
-const initialState = MARKETING
-
-export function marketing(state = initialState, action) {
-    /*switch (action.type) {
-        case START_CAMPAIGN:
-            return {...state, history: state.history.concat(action.addedCampaign)}
-        default:
-        return state
-    }*/
-    return state
-}
+import { CAMPAIGNS } from '../constants/MarketingConstants'
 
 const initialCampaigns = CAMPAIGNS
 
@@ -26,20 +14,8 @@ export function campaigns(state = initialCampaigns, action) {
                 campaignTemplate: action.campaignTemplate,
                 campaignMediaTemplate: action.campaignMediaTemplate
             }
-            return state.concat(addedCampaign)
+            return [addedCampaign, ...state]
         default:
         return state
     }
-}
-
-export function campaignMediaTypes(state = initialState, action) {
-    return state.campaignMedia
-}
-
-export function pressReleases(state = initialState, action) {
-    return state.pressReleases
-}
-
-export function marketResearch(state = initialState, action) {
-    return state.marketResearch
 }
