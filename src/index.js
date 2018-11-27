8cb4493bc51d92839fa95b30b594e65c382bb31a
Steffen Waldmann <swaldman@mail.uni-mannheim.de>
2018-11-28 00:12:56 +0100

    Silenced warning

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware, /*compose*/ } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import './index.css'
