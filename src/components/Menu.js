60a0777e46aa941d71ea944b25e9589029f6aab8
Steffen Waldmann <swaldman@mail.uni-mannheim.de>
2019-02-19 01:10:05 +0100

    Slightly updated menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -25,12 +25,18 @@ class Menu extends React.Component {
                     <h1>Capitalism X</h1>
                     <button className="large" onClick={this.props.newGame}>New Game</button>
                     <button className="large" onClick={this.props.loadGame}>Load Game</button>
-                    <div className="graphicsSettings">
-                        <p><b>Graphics</b></p>
+
+                    <h3><img className="icon" src={require("../static/icons/icons8-monitor.png")} alt="" />Graphics</h3>
+                    <p>
                         <button className={this.getGraphicsQuality() !== "low" ? "bordered" : null} onClick={(event) => { this.useLowGraphics(); window.location.reload();}}>Low</button>
                         <button className={this.getGraphicsQuality() !== "medium" ? "bordered" : null} onClick={(event) => { this.useMediumGraphics(); window.location.reload();}}>Medium</button>
                         <button className={this.getGraphicsQuality() !== "high" ? "bordered" : null} onClick={(event) => { this.useHighGraphics(); window.location.reload();}}>Best</button>
-                    </div>
+                    </p>
+                    <h3><img className="icon" src={require("../static/icons/icons8-hearts.png")} alt="" />Acknowledgements</h3>
+                    <p>
+                        <div>Some icons by <a href="https://icons8.com/" className="colorlessLink">Icons8</a>.</div>
+                        <div>Some icons by <a href="https://fontawesome.com" className="colorlessLink">Font Awesome</a>.</div>
+                    </p>
                 </div>
                 <div className="third">
                     <ul>
