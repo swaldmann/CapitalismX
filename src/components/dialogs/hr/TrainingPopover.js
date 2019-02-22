fdad68d6f0b48f9f6213552272ad04d3a998ae3e
Steffen Waldmann <swaldman@mail.uni-mannheim.de>
2019-02-22 03:16:47 +0100

    Fixed Training popover

--- src/components/dialogs/hr/TrainingPopover.js
+++ src/components/dialogs/hr/TrainingPopover.js
@@ -3,66 +3,85 @@ import TooltipTrigger from 'react-popper-tooltip'
 
 import {TRAINING_TEMPLATES} from '../../../constants/HRConstants'
 
-const TrainingPopover = ({employee, trainingIndex, actions}) => (
-    <TooltipTrigger
-        placement="top"
-        trigger="click"
-        tooltip={({
-          getTooltipProps,
-          getArrowProps,
-          tooltipRef,
-          arrowRef,
-          placement
-        }) => (
-          <div
-            {...getTooltipProps({
-              ref: tooltipRef,
-              className: 'tooltip-container'
-            })}
-          >
-            <div
-              {...getArrowProps({
-                ref: arrowRef,
-                'data-placement': placement,
-                className: 'tooltip-arrow'
-              })}
-            />
-                <div className="column-flexbox">
-                    <h3>Trainings</h3>
-                    <div className="borderedList">
-                        <ul>
-                            {TRAINING_TEMPLATES.map((trainingTemplate, trainingIndex) =>
-                                <li>
-                                    <div className="margin-bottom">
-                                        <button disabled={employee.skill + trainingTemplate.skillIncrease > 5} onClick={() => actions.trainEmployee(employee.index, trainingTemplate.skillIncrease, trainingTemplate.salaryIncreasePercentage)}>
+class TrainingPopover extends React.Component {
+    state = {
+        showsTooltip: false
+    }
 
-                                            <div className="flexbox">
-                                                <span className="cell-title content-size">{trainingTemplate.name}</span>
-                                                <span className="cell-detailTitle remaining-size">{trainingTemplate.cost.toLocaleString("en-US", {style: 'currency', currency: 'USD', minimumFractionDigits: 0, maximumFractionDigits: 0})}</span>
-                                            </div>
-                                            <div className="subtitle">{"Skill +" + trainingTemplate.skillIncrease + ", Salary +" + (trainingTemplate.salaryIncreasePercentage * 100) + "%"}</div>
-                                        </button>
-                                    </div>
-                                </li>
-                            )}
-                        </ul>
+    onVisibilityChange = (tooltipShown) => {
+        this.setState({ showsTooltip: tooltipShown })
+    }
+
+    trainEmployee = (employeeIndex, skillIncrease, salaryIncreasePercentage, actions) => {
+        this.setState({ showsTooltip: false })
+        actions.trainEmployee(employeeIndex, skillIncrease, salaryIncreasePercentage)
+    }
+
+    render() {
+        const {employee, /*trainingIndex,*/ actions} = this.props
+        return (
+        <TooltipTrigger
+            placement="top"
+            trigger="click"
+            tooltipShown={this.state.showsTooltip}
+            onVisibilityChange={this.onVisibilityChange}
+            tooltip={({
+              getTooltipProps,
+              getArrowProps,
+              tooltipRef,
+              arrowRef,
+              placement
+            }) => (
+              <div
+                {...getTooltipProps({
+                  ref: tooltipRef,
+                  className: 'tooltip-container'
+                })}
+              >
+                <div
+                  {...getArrowProps({
+                    ref: arrowRef,
+                    'data-placement': placement,
+                    className: 'tooltip-arrow'
+                  })}
+                />
+                    <div className="column-flexbox">
+                        <h3>Trainings</h3>
+                        <div className="borderedList">
+                            <ul>
+                                {TRAINING_TEMPLATES.map((trainingTemplate, trainingIndex) =>
+                                    <li>
+                                        <div className="margin-bottom">
+                                            <button disabled={employee.skill + trainingTemplate.skillIncrease > 5} onClick={() => this.trainEmployee(employee.index, trainingTemplate.skillIncrease, trainingTemplate.salaryIncreasePercentage, actions)}>
+
+                                                <div className="flexbox">
+                                                    <span className="cell-title content-size">{trainingTemplate.name}</span>
+                                                    <span className="cell-detailTitle remaining-size">{trainingTemplate.cost.toLocaleString("en-US", {style: 'currency', currency: 'USD', minimumFractionDigits: 0, maximumFractionDigits: 0})}</span>
+                                                </div>
+                                                <div className="subtitle">{"Skill +" + trainingTemplate.skillIncrease + ", Salary +" + (trainingTemplate.salaryIncreasePercentage * 100) + "%"}</div>
+                                            </button>
+                                        </div>
+                                    </li>
+                                )}
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-        )}
-      >
-        {({ getTriggerProps, triggerRef }) => (
-          <button
-            {...getTriggerProps({
-              ref: triggerRef,
-              className: 'trigger centered constructive'
-              /* your props here */
-            })}
+            )}
           >
-            Train
-            </button>
-        )}
-     </TooltipTrigger>
-)
+            {({ getTriggerProps, triggerRef }) => (
+              <button
+                {...getTriggerProps({
+                  ref: triggerRef,
+                  className: 'trigger centered constructive'
+                  /* your props here */
+                })}
+              >
+                Train
+                </button>
+            )}
+         </TooltipTrigger>
+    )}
+}
 
 export default TrainingPopover
